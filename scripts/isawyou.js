764db8cd2b8d2b9b2409ba8fca0537d138c9ce86
Claire Nord <cnord@mit.edu>
2019-10-25 16:50:49 -0400

    isawyou: eslint

--- scripts/isawyou.js
+++ scripts/isawyou.js
@@ -14,10 +14,13 @@
 // Author:
 //   Anthony Liu, wiredfool, patcon@gittip
 
+// config
+const config = require('hubot-conf');
+
 // helpers
-function bind(fn, context) {
-  return function() {
-    return fn.apply(context, arguments);
+function bind(fn, context, ...args) {
+  return function apply() {
+    return fn.apply(context, args);
   };
 }
 
@@ -52,23 +55,22 @@ class ISawYou {
 
   // adds this message to the msg's user's count if its a pic
   add(msg) {
-    var username = msg.message.user.name;
+    const username = msg.message.user.name;
     if (this.isPictureMessage(msg)) {
       this.robot.logger.debug('isawyou.add #{username}');
-      if (this.cache.hasOwnProperty(username)) {
+      if (username in this.cache) {
         this.cache[username] += 1;
       } else {
         this.cache[username] = 1;
       }
       return true;
-    } else {
-      return false;
     }
+    return false;
   }
 
   // increments username's points
   increment(username) {
-    if (this.cache.hasOwnProperty(username)) {
+    if (username in this.cache) {
       this.cache[username] += 1;
     } else {
       this.cache[username] = 1;
@@ -77,7 +79,7 @@ class ISawYou {
 
   // decrements username's points
   decrement(username) {
-    if (this.cache.hasOwnProperty(username)) {
+    if (username in this.cache) {
       this.cache[username] -= 1;
       if (this.cache[username] < 0) {
         this.cache[username] = 0;
@@ -95,11 +97,10 @@ class ISawYou {
   // returns the specific user's count
   getCount(username) {
     this.robot.logger.debug('isawyou.get #{username}');
-    if (this.cache.hasOwnProperty(username)) {
+    if (username in this.cache) {
       return this.cache[username];
-    } else {
-      return 0;
     }
+    return 0;
   }
 
   // return the top k-scoring users
@@ -107,19 +108,14 @@ class ISawYou {
   getTopK(k) {
     // yes there are more efficient ways to do this but
     // practically it really isn't going to make a differnce
-    var self = this;
+    const self = this;
     return Object.keys(this.cache)
-      .map(function(username) {
-        return {
-          username: username,
-          points: self.cache[username]
-        };
-      }).sort(function(a, b) {
+      .map((username) => ({ username, points: self.cache[username] }))
+      .sort((a, b) => {
         if (a.points === b.points) {
           return a.username > b.username ? 1 : -1;
-        } else {
-          return b.points - a.points;  
         }
+        return b.points - a.points;
       }).slice(0, k);
   }
 
@@ -129,30 +125,27 @@ class ISawYou {
   }
 }
 
-module.exports = function(robot) {
-  'use strict';
-
-  // config
-  var config = require('hubot-conf')('isawyou', robot);
+module.exports = (robot) => {
+  const robotConfig = config('isawyou', robot);
 
   // working variables
-  var iSawYou = new ISawYou(robot);
+  const iSawYou = new ISawYou(robot);
 
   function reportCount(msg, username) {
-    var count = iSawYou.getCount(username);
-    msg.send('*' + username + '* has ' + count + ' i-saw-you points.');
+    const count = iSawYou.getCount(username);
+    msg.send(`*${username}* has ${count} i-saw-you points.`);
   }
 
   // update i-saw-you counts
-  robot.hear(/.*/, function(msg) {
+  robot.hear(/.*/, (msg) => {
     // only listen to messages in the #i-saw-you channel
-    if (config('room') === msg.message.room) {
-      var username = msg.message.user.name;
-      var matchesInc = msg.message.text.match(
-        /^@([-\w.\\^|{}`\[\]]+)\+\+$/
+    if (robotConfig('room') === msg.message.room) {
+      const username = msg.message.user.name;
+      const matchesInc = msg.message.text.match(
+        /^@([-\w.\\^|{}`[\]]+)\+\+$/,
       );
-      var matchesDec = msg.message.text.match(
-        /^@([-\w.\\^|{}`\[\]]+)--$/
+      const matchesDec = msg.message.text.match(
+        /^@([-\w.\\^|{}`[\]]+)--$/,
       );
       if (matchesInc && matchesInc.length === 2) {
         iSawYou.increment(matchesInc[1]);
@@ -161,7 +154,7 @@ module.exports = function(robot) {
         iSawYou.decrement(matchesDec[1]);
         reportCount(msg, matchesDec[1]);
       } else {
-        var added = iSawYou.add(msg);
+        const added = iSawYou.add(msg);
         if (added) {
           reportCount(msg, username);
         }
@@ -171,51 +164,48 @@ module.exports = function(robot) {
 
   // respond to i-saw-you increments
   robot.respond(
-    /isawyou @?([-\w.\\^|{}`\[\]]+)\+\+$/,
-    function(msg) {
-      var username = msg.match[1];
+    /isawyou @?([-\w.\\^|{}`[\]]+)\+\+$/,
+    (msg) => {
+      const username = msg.match[1];
       iSawYou.increment(username);
       reportCount(msg, username);
-    }
+    },
   );
 
   // respond to i-saw-you leaderboard requests
   robot.respond(
     /isawyou leaderboard/,
-    function(msg) {
-      var k = parseInt(config('k'));
-      var users = iSawYou.getTopK(k);
-      var prefix = '~ i-saw-you leaderboard ~\n';
-      var message = prefix + users
-        .reduce(function(a, user) {
-          return a + '*' + user.username + '*: ' +
-            user.points + ' points\n';
-        }, '');
+    (msg) => {
+      const k = parseInt(robotConfig('k'), 10);
+      const users = iSawYou.getTopK(k);
+      const prefix = '~ i-saw-you leaderboard ~\n';
+      const message = prefix + users
+        .reduce((a, user) => `${a}*${user.username}*: ${user.points} points\n`, '');
       msg.send(message);
-    }
+    },
   );
 
   // respond to i-saw-you get requests
   robot.respond(
-    /isawyou get @?([-\w.\\^|{}`\[\]]+)$/,
-    function(msg) {
-      var username = msg.match[1];
+    /isawyou get @?([-\w.\\^|{}`[\]]+)$/,
+    (msg) => {
+      const username = msg.match[1];
       reportCount(msg, username);
-    }
+    },
   );
 
   // respond to point set requests
   robot.respond(
-    /isawyou set @?([-\w.\\^|{}`\[\]]+) (\d+)/,
-    function(msg) {
+    /isawyou set @?([-\w.\\^|{}`[\]]+) (\d+)/,
+    (msg) => {
       if (msg.match.length < 3) {
         msg.send('Usage: hackbot isawyou set <user> <number>');
       } else {
-        var username = msg.match[1];
-        var newPoints = parseInt(msg.match[2]);
+        const username = msg.match[1];
+        const newPoints = parseInt(msg.match[2], 10);
         iSawYou.set(username, newPoints);
-        msg.send('*' + username + '* now has ' + newPoints + ' i-saw-you points.');
+        msg.send(`*${username}* now has ${newPoints} i-saw-you points.`);
       }
-    }
+    },
   );
 };
