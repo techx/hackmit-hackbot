5aa9f6325584b3a5284a3f25c043a5e2e948a374
Claire Nord <cnord@mit.edu>
2019-11-09 20:29:31 -0500

    util: eslint

--- scripts/util.js
+++ scripts/util.js
@@ -9,43 +9,36 @@
  * DS203: Remove `|| {}` from converted for-own loops
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-const shallowClone = function(obj) {
-  const copy = {};
-  for (let key of Object.keys(obj || {})) {
-    const value = obj[key];
-    copy[key] = value;
-  }
-  copy.__proto__ = obj.__proto__; // not standard in ECMAScript, but it works
-  return copy;
-};
+let rangeFn;
 
-module.exports = function(robot) {
-  const config = require("hubot-conf")("util", robot);
+module.exports = (robot) => {
+  robot.respond(/clear$/, (res) => res.send(
+    rangeFn(1, 60, true)
+      .map(() => '.')
+      .join('\n'),
+  ));
 
-  robot.respond(/clear$/, res =>
-    res.send(
-      __range__(1, 60, true)
-        .map(n => ".")
-        .join("\n")
-    )
-  );
-
-  if (robot.adapterName === "slack") {
-    robot.logger.info("Adapter is slack: will terminate on client close");
-    robot.adapter.client.on("close", () => process.exit(0));
+  if (robot.adapterName === 'slack') {
+    robot.logger.info('Adapter is slack: will terminate on client close');
+    robot.adapter.client.on('close', () => process.exit(0));
   } else {
-    robot.logger.info("Adapter is not slack, will not terminate on client close");
+    robot.logger.info('Adapter is not slack, will not terminate on client close');
   }
 
-  return robot.respond(/echo2 ((.*\s*)+)/i, res => res.send(res.match[1]));
+  return robot.respond(/echo2 ((.*\s*)+)/i, (res) => res.send(res.match[1]));
 };
 
-function __range__(left, right, inclusive) {
-  let range = [];
-  let ascending = left < right;
-  let end = !inclusive ? right : ascending ? right + 1 : right - 1;
-  for (let i = left; ascending ? i < end : i > end; ascending ? i++ : i--) {
+rangeFn = (left, right, inclusive) => {
+  const range = [];
+  const ascending = left < right;
+  let end;
+  if (!inclusive) {
+    end = right;
+  } else {
+    end = ascending ? right + 1 : right - 1;
+  }
+  for (let i = left; ascending ? i < end : i > end; ascending ? i += 1 : i -= 1) {
     range.push(i);
   }
   return range;
-}
+};
