fac0c2a5e988626ef3ca8ef7ea0825f80c2c451d
Claire Nord <cnord@mit.edu>
2019-11-09 20:20:18 -0500

    sponsor: eslint

--- scripts/sponsor.js
+++ scripts/sponsor.js
@@ -22,302 +22,245 @@
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-const util = require("util");
-const Spreadsheet = require("google-spreadsheet");
-const timeago = require("timeago");
-const streakapi = require("streakapi");
+const config = require('hubot-conf');
+const Spreadsheet = require('google-spreadsheet');
+const timeago = require('timeago');
+const streakapi = require('streakapi');
 
-const findMatchingRow = function(rows, companyName) {
-  for (let row of Array.from(rows)) {
-    if (companyName.toLowerCase() === row[SPONSOR_NAME_COL].toLowerCase()) {
-      return row;
-    }
-  }
-};
+const LEVEL_COL = '_cre1l';
+const SPONSOR_NAME_COL = 'forhackbotmoneypleasedonoteditthisrow';
 
-var SPONSOR_NAME_COL = "company";
-const LEVEL_COL = "level";
+const findMatchingRow = (rows, companyName) => rows.find(
+  (row) => companyName.toLowerCase() === row[SPONSOR_NAME_COL].toLowerCase(),
+);
 
 // 2017 levels from sponsor spreadsheet
 const LEVELS = [
-  "Custom",
-  "Platinum",
-  "Gold",
-  "Silver",
-  "Bronze",
-  "Startup",
-  "NotSponsoring"
+  'Custom',
+  'Platinum',
+  'Gold',
+  'Silver',
+  'Bronze',
+  'Startup',
+  'NotSponsoring',
 ];
 
 // 2017 keys and statuses from Streak
 let STATUSES = {
-  "5001": "To Email",
-  "5002": "To Respond",
-  "5003": "Initial email",
-  "5004": "Talking",
-  "5005": "Invoiced",
-  "5006": "Paid",
-  "5007": "Rejected",
-  "5008": "Pinged"
+  5001: 'To Email',
+  5002: 'To Respond',
+  5003: 'Initial email',
+  5004: 'Talking',
+  5005: 'Invoiced',
+  5006: 'Paid',
+  5007: 'Rejected',
+  5008: 'Pinged',
 };
 
-const creds = require("../hackmit-money-2015-credentials.json");
+const creds = require('../hackmit-money-2015-credentials.json');
 
 // Emails from these addresses are not company contacts.
 const EMAIL_FILTER = /^(?!.*(@mit\.edu|@hackmit\.org|@gmail\.com)).*/i;
 
-const filterContacts = function(allContacts) {
-  const contacts = [];
-  for (let contact of Array.from(allContacts)) {
-    if (EMAIL_FILTER.test(contact)) {
-      contacts.push(contact);
-    }
+const filterContacts = (allContacts) => allContacts.filter((contact) => EMAIL_FILTER.test(contact));
+
+const getCompanyRows = (sheet, callback) => sheet.useServiceAccountAuth(creds, (err) => {
+  if (err) {
+    return callback(err);
   }
-  return contacts;
-};
-
-const getCompanyRows = (sheet, callback) =>
-  sheet.useServiceAccountAuth(creds, function(err) {
-    if (err) {
-      return callback(err);
-    } else {
-      return sheet.getInfo(function(err, info) {
-        if (err) {
-          return callback(err);
-        } else {
-          const companyStatusSheet = info.worksheets[0];
-          return companyStatusSheet.getRows(callback);
-        }
-      });
+  return sheet.getInfo((err2, info) => {
+    if (err2) {
+      return callback(err2);
     }
+    const companyStatusSheet = info.worksheets[0];
+    return companyStatusSheet.getRows(callback);
   });
+});
 
-const getCompanyRow = (sheet, res, callback) =>
-  getCompanyRows(sheet, function(err, rows) {
-    if (err) {
-      return callback(err);
-    } else {
-      const companyName = res.match[1];
-      const update = res.match[2];
-      const row = findMatchingRow(rows, companyName);
-      return callback(null, row, companyName, update);
-    }
-  });
+const getCompanyRow = (sheet, res, callback) => getCompanyRows(sheet, (err, rows) => {
+  if (err) {
+    return callback(err);
+  }
+  const companyName = res.match[1];
+  const update = res.match[2];
+  const row = findMatchingRow(rows, companyName);
+  return callback(null, row, companyName, update);
+});
 
-const getPipeline = (streak, callback) =>
-  streak.Pipelines.getAll()
-    .then((
-      pipelines // Most recent pipeline is index 0
-    ) => callback(null, pipelines[0]))
-    .catch(err => callback(err));
+const getPipeline = (streak, callback) => streak.Pipelines.getAll()
+  .then((
+    pipelines, // Most recent pipeline is index 0
+  ) => callback(null, pipelines[0]))
+  .catch((err) => callback(err));
 
-const formatBox = function(box) {
+const formatBox = (box) => {
   let date;
   try {
     date = new Date(box.lastEmailReceivedTimestamp).toISOString().slice(0, 10);
   } catch (err) {
-    date = "Never :cry:";
+    date = 'Never :cry:';
   }
+  const assignee = box.assignedToSharingEntries.length > 0 ? box.assignedToSharingEntries[0].fullName : 'No one :cry';
   return `*${box.name}*
 :question: ${STATUSES[box.stageKey]}
-:point_right: ${box.assignedToSharingEntries[0].fullName}
-:e-mail: ${filterContacts(box.emailAddresses).join(", ")}
+:point_right: ${assignee}
+:e-mail: ${filterContacts(box.emailAddresses).join(', ')}
 :alarm_clock: ${date}
-:pencil: ${box.notes ? box.notes : ""}`;
+:pencil: ${box.notes ? box.notes : ''}`;
 };
 
-module.exports = function(robot) {
-  const config = require("hubot-conf")("money", robot);
+module.exports = (robot) => {
+  const conf = config('money', robot);
 
-  const streak = function() {
-    const streakKey = config("streak.key");
+  const streak = () => {
+    const streakKey = conf('streak.key');
     const str = new streakapi.Streak(streakKey);
     return str;
   };
 
   const boxes = { data: null, time: null };
 
-  const getStatuses = function(callback) {
+  const getStatuses = (callback) => {
     const str = streak();
-    return getPipeline(str, function(err, pipeline) {
+    return getPipeline(str, (err, pipeline) => {
       if (err) {
         return callback(err);
-      } else {
-        const statuses = {};
-        for (let s in pipeline.stages) {
-          statuses[s] = pipeline.stages[s].name;
-        }
-        return callback(null, statuses);
       }
+      const statuses = {};
+      Object.keys(pipeline.stages).map((s) => {
+        statuses[s] = pipeline.stages[s].name;
+        return s;
+      });
+      return callback(null, statuses);
     });
   };
 
-  const getBoxes = function(callback) {
+  const getBoxes = (callback) => {
     const str = streak();
-    return getPipeline(str, function(err, pipeline) {
+    return getPipeline(str, (err, pipeline) => {
       if (err) {
         return callback(err);
-      } else {
-        return str.Boxes.getForPipeline(pipeline.pipelineKey)
-          .then(function(data) {
-            boxes.data = data;
-            // TODO: get last updated time
-            boxes.time = new Date();
-            if (callback != null) {
-              return callback(null, boxes);
-            }
-          })
-          .catch(err => callback(err));
       }
+      return str.Boxes.getForPipeline(pipeline.pipelineKey)
+        .then((data) => {
+          boxes.data = data;
+          // TODO: get last updated time
+          boxes.time = new Date();
+          if (callback != null) {
+            callback(null, boxes);
+          }
+        })
+        .catch((err2) => callback(err2));
     });
   };
 
   setInterval(getBoxes, 3 * 60 * 1000);
 
-  const getBoxesOrCache = function(res, callback) {
+  const getBoxesOrCache = (res, callback) => {
     if (boxes.data != null) {
       return callback(null, boxes);
-    } else {
-      return getBoxes(callback);
     }
+    return getBoxes(callback);
   };
 
-  const print = function(res, text) {
+  const print = (res, text) => {
     const delta = timeago(boxes.time);
-    let message = "_Up to date as of " + delta + "._\n";
+    let message = `_Up to date as of ${delta}._\n`;
     message += text;
     return res.send(message);
   };
 
-  const printBoxes = (res, boxsubset) =>
+  const printBoxes = (res, boxsubset) => {
     print(
       res,
-      (() => {
-        const result = [];
-        for (let box in boxsubset) {
-          result.push(formatBox(boxsubset[box]));
-        }
-        return result;
-      })().join("\n") + `\n_Total: ${boxsubset ? Object.keys(boxsubset).length : 0}_`
+      `${Object.values(boxsubset).map(formatBox).join('\n')}\n_Total: ${boxsubset ? Object.keys(boxsubset).length : 0}_`,
     );
+  };
 
-  robot.respond(/sponsor fetch$/i, res =>
-    getBoxes(function(err, res) {
-      if (err) {
-        return res.send(`Error while getting boxes: ${err}`);
-      } else {
-        return res.send("Fetched new data!");
-      }
-    })
-  );
+  robot.respond(/sponsor fetch$/i, (res) => getBoxes((err) => {
+    if (err) {
+      return res.send(`Error while getting boxes: ${err}`);
+    }
+    return res.send('Fetched new data!');
+  }));
 
-  getStatuses(function(err, stats) {
+  getStatuses((err, stats) => {
     if (err) {
       // fails silently
       return console.log(`Error while getting statuses: ${err}`);
-    } else {
-      return (STATUSES = stats);
     }
+    STATUSES = stats;
+    return STATUSES;
   });
 
-  const spreadsheetUrl = config("spreadsheet.url");
+  const spreadsheetUrl = conf('spreadsheet.url');
   const sheet = new Spreadsheet(spreadsheetUrl);
 
   // Get a link to the spreadsheet
-  robot.respond(/sponsor spreadsheet/i, res =>
-    res.send("https://go.hackmit.org/sponsor")
-  );
+  robot.respond(/sponsor spreadsheet/i, (res) => res.send('https://go.hackmit.org/sponsor'));
 
   // Returns a list of companies with the given status
   robot.respond(
     new RegExp(
-      "sponsor (" +
-        (() => {
-          const result = [];
-          for (let k of Object.keys(STATUSES || {})) {
-            const v = STATUSES[k];
-            result.push(v);
-          }
-          return result;
-        })().join("|") +
-        ")$",
-      "i"
+      `sponsor (${Object.values(STATUSES).join('|')})$`,
+      'i',
     ),
-    res =>
-      getBoxesOrCache(res, function(err, boxes) {
-        if (err) {
-          return res.send(`Error getting boxes: ${err}`);
-        } else {
-          const status = res.match[1];
-          const companies = [];
-          for (let box of Array.from(boxes.data)) {
-            if (status.toLowerCase() === STATUSES[box.stageKey].toLowerCase()) {
-              companies.push(box.name);
-            }
-          }
-          const join = companies.length < 15 ? "\n" : ", ";
-          return print(res, `${companies.join(join)}\n_Total: ${companies.length}_`);
-        }
-      })
+    (res) => getBoxesOrCache(res, (err, newBoxes) => {
+      if (err) {
+        return res.send(`Error getting boxes: ${err}`);
+      }
+      const status = res.match[1];
+      const companies = newBoxes.data.filter(
+        (box) => status.toLowerCase() === STATUSES[box.stageKey].toLowerCase(),
+      ).map((box) => box.name);
+      const join = companies.length < 15 ? '\n' : ', ';
+      return print(res, `${companies.join(join)}\n_Total: ${companies.length}_`);
+    }),
   );
 
   // Returns a list of companies with the given tier
-  robot.respond(new RegExp("sponsor (" + LEVELS.join("|") + ")$", "i"), res =>
-    getCompanyRows(sheet, function(err, rows) {
-      if (err) {
-        return res.send(`Error while getting company rows: ${err}`);
-      } else {
-        const level = res.match[1];
-        const companies = [];
-        for (let row of Array.from(rows)) {
-          if (level.toLowerCase() === row[LEVEL_COL].toLowerCase().substring(1)) {
-            companies.push(row[SPONSOR_NAME_COL]);
-          }
-        }
-        return res.send(`*Total:* ${companies.length}\n${companies.join("\n")}`);
-      }
-    })
-  );
+  robot.respond(new RegExp(`sponsor (${LEVELS.join('|')})$`, 'i'), (res) => getCompanyRows(sheet, (err, rows) => {
+    if (err) {
+      return res.send(`Error while getting company rows: ${err}`);
+    }
+    const level = res.match[1].toLowerCase();
+    const companies = rows.filter(
+      (row) => row[LEVEL_COL] !== undefined && row[LEVEL_COL].toLowerCase() === level,
+    ).map((row) => row[SPONSOR_NAME_COL]);
+    return res.send(`*Total:* ${companies.length}\n${companies.join('\n')}`);
+  }));
 
   // Update sponsor tier
-  robot.respond(/sponsor level (.*) ([A-Za-z0-9]+)/i, res =>
-    getCompanyRow(sheet, res, function(err, row, company, update) {
-      if (err) {
-        return res.send(`Error while getting company row: ${err}`);
-      } else if (!row) {
-        return res.send("Didn't find matching company");
-      } else {
-        if (!Array.from(LEVELS).includes(update)) {
-          return res.send(`Please provide a valid level:\n${LEVELS.join("\n")}`);
-        } else {
-          row[LEVEL_COL] = update;
-          return row.save(function(err) {
-            if (err) {
-              return res.send(`Error while updating cell value: ${err}`);
-            } else {
-              return res.send(`Successfully updated ${company}`);
-            }
-          });
-        }
+  robot.respond(/sponsor level (.*) ([A-Za-z0-9]+)/i, (res) => getCompanyRow(sheet, res, (err, row, company, update) => {
+    if (err) {
+      return res.send(`Error while getting company row: ${err}`);
+    } if (!row) {
+      return res.send("Didn't find matching company");
+    }
+    if (!Array.from(LEVELS).includes(update)) {
+      return res.send(`Please provide a valid level:\n${LEVELS.join('\n')}`);
+    }
+    const newRow = row;
+    newRow[LEVEL_COL] = update;
+    return newRow.save((err2) => {
+      if (err2) {
+        return res.send(`Error while updating cell value: ${err2}`);
       }
-    })
-  );
+      return res.send(`Successfully updated ${company}`);
+    });
+  }));
 
-  return robot.respond(/sponsor info (.*)/i, res =>
-    getBoxesOrCache(res, function(err, boxes) {
-      if (err) {
-        return res.send(`Error getting boxes: ${err}`);
-      } else {
-        const search = res.match[1];
-        const re = new RegExp("^" + search.toLowerCase(), "i");
-        const companies = {};
-        for (let box of Array.from(boxes.data)) {
-          if (re.test(box.name.toLowerCase())) {
-            companies[box.name] = box;
-          }
-        }
-        return printBoxes(res, companies);
-      }
-    })
-  );
+  return robot.respond(/sponsor info (.*)/i, (res) => getBoxesOrCache(res, (err, newBoxes) => {
+    if (err) {
+      return res.send(`Error getting boxes: ${err}`);
+    }
+    const search = res.match[1];
+    const re = new RegExp(`^${search.toLowerCase()}`, 'i');
+    const companies = {};
+    newBoxes.data.filter((box) => re.test(box.name.toLowerCase())).map((box) => {
+      companies[box.name] = box;
+      return box;
+    });
+    return printBoxes(res, companies);
+  }));
 };
