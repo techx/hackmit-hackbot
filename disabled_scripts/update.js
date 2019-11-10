c15304f6b41e0c6310379004a0455fe0efb586c7
Claire Nord <cnord@mit.edu>
2019-11-09 23:11:02 -0500

    update: eslint, incorporate decaffeinate recommendations

--- disabled_scripts/update.js
+++ disabled_scripts/update.js
@@ -1,11 +1,7 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 // Description:
 //   Allows hubot to update itself using git pull and npm update.
-//   If updates are downloaded you'll need to restart hubot, for example using "hubot die" (restart using a watcher like forever.js).
+//   If updates are downloaded you'll need to restart hubot, for example using "hubot
+//   die" (restart using a watcher like forever.js).
 //
 //   Shamelessly stolen from: https://github.com/github/hubot-scripts/blob/master/src/scripts/update.coffee
 //   ... with some slight modifications.
@@ -22,66 +18,62 @@
 // Author:
 //   benjamine, Detry322
 
-const child_process = require('child_process');
-const downloaded_updates = false;
+const childProcess = require('child_process');
 
-const restart = function(res) {
-  res.send("Restarting...");
-  return setTimeout(() => process.exit()
-  , 500); // Give process some time to send message
+const restart = (res) => {
+  res.send('Restarting...');
+  setTimeout(() => process.exit(),
+    500); // Give process some time to send message
 };
 
-const send = function(res, should_send, message) {
-  if (should_send) {
-    return res.send(message);
+const send = (res, shouldSend, message) => {
+  if (shouldSend) {
+    res.send(message);
   }
 };
 
-const update = function(res, send_std, send_err) {
+const update = (res, sendStd, sendErr) => {
   try {
-    send(res, send_std, "fetching latest source code...");
-    return child_process.exec('git fetch --all >/dev/null 2>&1 && git log --oneline --graph --stat HEAD..@{u} && git reset --hard @{u}', function(error, stdout, stderr) {
+    send(res, sendStd, 'fetching latest source code...');
+    childProcess.exec('git fetch --all >/dev/null 2>&1 && git log --oneline --graph --stat HEAD..@{u} && git reset --hard @{u}', (error, stdout, stderr) => {
       if (error) {
-        send(res, send_err, "git fetch/reset failed: ```" + stderr + "```");
+        send(res, sendErr, `git fetch/reset failed: \`\`\`${stderr}\`\`\``);
       } else {
-        send(res, send_std, `\`\`\`${stdout}\`\`\``);
+        send(res, sendStd, `\`\`\`${stdout}\`\`\``);
       }
       try {
-        send(res, send_std, "npm update...");
-        return child_process.exec('npm update', function(error, stdout, stderr) {
-          if (error) {
-            send(res, send_err, "npm update failed: ```" + stderr + "```");
+        send(res, sendStd, 'npm update...');
+        childProcess.exec('npm update', (error1, stdout1, stderr1) => {
+          if (error1) {
+            send(res, sendErr, `npm update failed: \`\`\`${stderr1}\`\`\``);
           } else {
-            const output = stdout+'';
+            const output = `${stdout1}`;
             if (/node_modules/.test(output)) {
-              send(res, send_std, "some dependencies updated:\n```" + output + "```");
+              send(res, sendStd, `some dependencies updated:\n\`\`\`${output}\`\`\``);
             } else {
-              send(res, send_std, "all dependencies are up-to-date");
+              send(res, sendStd, 'all dependencies are up-to-date');
             }
           }
-          return restart(res);
+          restart(res);
         });
       } catch (error1) {
-        error = error1;
-        return send(res, send_err, "npm update failed: " + error);
+        send(res, sendErr, `npm update failed: ${error1}`);
       }
     });
   } catch (error1) {
-    const error = error1;
-    return send(res, send_err, "git pull failed: " + error);
+    send(res, sendErr, `git pull failed: ${error1}`);
   }
 };
 
-module.exports = function(robot) {
+module.exports = (robot) => {
+  robot.respond(/restart( yourself)?$/i, (res) => restart(res));
 
-  robot.respond(/restart( yourself)?$/i, res => restart(res));
-
-  robot.respond(/update silent$/i, function(res) {
-    res.send("Updating...");
+  robot.respond(/update silent$/i, (res) => {
+    res.send('Updating...');
     return update(res, false, true);
   });
 
-  robot.respond(/update super silent$/i, res => update(res, false, false));
+  robot.respond(/update super silent$/i, (res) => update(res, false, false));
 
-  return robot.respond(/update( yourself)?$/i, res => update(res, true, true));
+  robot.respond(/update( yourself)?$/i, (res) => update(res, true, true));
 };
