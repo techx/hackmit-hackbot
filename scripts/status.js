109571d261cd8347a9fcd01a99998d1a377b2a3d
Claire Nord <cnord@mit.edu>
2019-11-09 20:59:41 -0500

    status: eslint, incorporate decaf suggestions

--- scripts/status.js
+++ scripts/status.js
@@ -9,12 +9,4 @@
 //
 // Author:
 //   Detry322
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-module.exports = robot =>
-  robot.router.get("/status/ping", (req, res) =>
-    res.header("Content-Type", "text/plain").send("Pong!")
-  );
+module.exports = (robot) => robot.router.get('/status/ping', (req, res) => res.header('Content-Type', 'text/plain').send('Pong!'));
