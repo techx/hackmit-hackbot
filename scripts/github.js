3a98da2f45be0ecbfe9d7491d4c549abbf521277
Claire Nord <cnord@mit.edu>
2019-11-09 22:53:02 -0500

    github: upgrade to @octokit/rest, eslint, incorporate decaffeinate recs

--- scripts/github.js
+++ scripts/github.js
@@ -2,52 +2,39 @@
 //   GitHub stuff.
 //
 // Configuration:
-//   HUBOT_GITHUB_USERNAME
-//   HUBOT_GITHUB_PASSWORD
+//   HUBOT_GITHUB_TOKEN - Personal access token for the account
 //   HUBOT_GITHUB_ORGANIZATION
 //
 // Commands:
 //   hubot github issue create <repo> "<title>" "<description>"
 //
 // Author:
-//   anishathalye
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
+//   anishathalye, cmnord
 
-const GitHubApi = require("github");
+const config = require('hubot-conf');
+const Octokit = require('@octokit/rest');
 
-module.exports = function(robot) {
-  const config = require("hubot-conf")("github", robot);
+module.exports = (robot) => {
+  const conf = config('github', robot);
 
-  const github = function() {
-    const user = config("username");
-    const pass = config("password");
-
-    const gh = new GitHubApi({ version: "3.0.0" });
-    gh.authenticate({ type: "basic", username: user, password: pass });
-    return gh;
+  const github = () => {
+    const pass = conf('password');
+    return new Octokit({ auth: pass });
   };
 
   // using '[\s\S]' to match multiline (instead of just '.')
   return robot.respond(
     /github issue create ([A-Za-z0-9_.-]+) "(.+)" "([\s\S]+)"/i,
-    function(res) {
+    (res) => {
       const creator = res.message.user.name;
-      const repo = res.match[1];
-      const title = res.match[2];
-      const desc = `${res.match[3]}\n\n(submitted by ${creator})`;
-      const org = config("organization");
+      const [, repo, title, body] = res.match;
+      const desc = `${body}\n\n(submitted by ${creator})`;
+      const org = conf('organization');
       const { issues } = github();
-      return issues.create({ user: org, repo, title, body: desc }, function(err, data) {
-        if (!err) {
-          return res.send(`Created issue #${data.number} in ${org}/${repo}.`);
-        } else {
-          return res.send("Error creating issue.");
-        }
-      });
-    }
+      issues.create({
+        owner: org, repo, title, body: desc,
+      }).then((issueRes) => res.send(`Created issue #${issueRes.data.number} in ${org}/${repo}.`))
+        .catch((err) => res.send(`Error creating issue: ${err}.`));
+    },
   );
 };
