5e630687963f16529194cdc3da88347ce48c1a50
Claire Nord <cnord@mit.edu>
2019-11-09 21:57:58 -0500

    warriors_game: eslint, incorporate decaffeinate suggestions, fix link

--- disabled_scripts/warriors_game.js
+++ disabled_scripts/warriors_game.js
@@ -1,8 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 // Description:
 //   Warriors game score
 //
@@ -11,35 +6,39 @@
 
 const querystring = require('querystring');
 
-module.exports = function(robot) {
+let rangeFn;
 
-  const config = require('hubot-conf')('util', robot);
+module.exports = (robot) => {
+  robot.respond(/clear$/, (res) => res.send((rangeFn(1, 60, true).map(() => '.')).join('\n')));
 
-  robot.respond(/clear$/, res => res.send((__range__(1, 60, true).map((n) => ".")).join("\n")));
+  if (robot.adapterName === 'slack') {
+    robot.logger.info('Adapter is slack: will terminate on client close');
+    robot.adapter.client.on('close', () => process.exit(0));
+  } else {
+    robot.logger.info('Adapter is not slack, will not terminate on client close');
+  }
 
-  if (robot.adapterName === "slack") {
-      robot.logger.info("Adapter is slack: will terminate on client close");
-      robot.adapter.client.on('close', () => process.exit(0));
-    } else {
-      robot.logger.info("Adapter is not slack, will not terminate on client close");
-    }
-
-  return robot.respond(/warriors score/i, res => robot.http("http://espn.go.com/nba/bottomline/scores").get()(function(err, result, body) {
+  robot.respond(/warriors score/i, (res) => robot.http('http://www.espn.com/nba/bottomline/scores').get()((err, result, body) => {
     if (err || (result.statusCode !== 200)) {
-      res.send("Had trouble getting the score :(");
+      res.send('Had trouble getting the score :(');
       return;
     }
     const qs = querystring.parse(body);
-    return res.send(qs.nba_s_left1);
+    res.send(qs.nba_s_left1);
   }));
 };
 
-function __range__(left, right, inclusive) {
-  let range = [];
-  let ascending = left < right;
-  let end = !inclusive ? right : ascending ? right + 1 : right - 1;
-  for (let i = left; ascending ? i < end : i > end; ascending ? i++ : i--) {
+rangeFn = (left, right, inclusive) => {
+  const range = [];
+  const ascending = left < right;
+  let end;
+  if (!inclusive) {
+    end = right;
+  } else {
+    end = ascending ? right + 1 : right - 1;
+  }
+  for (let i = left; ascending ? i < end : i > end; ascending ? i += 1 : i -= 1) {
     range.push(i);
   }
   return range;
-}
+};
